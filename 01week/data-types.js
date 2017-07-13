fa43fcf6656f1210971e4d7c4608db60696a4ca4
Mar10God1 <mar10_godwin@yahoo.com>
2017-07-12 19:34:08 -0500

    rock paper scissors

--- 01week/data-types.js
+++ 01week/data-types.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 function getDate () {
   return Date();
--- 01week/rockPaperScissors.js
+++ 01week/rockPaperScissors.js
@@ -9,9 +9,33 @@ const rl = readline.createInterface({
 
 
 function rockPaperScissors(hand1, hand2) {
-
+hand1 = hand1.toLowerCase().trim();
+hand2 = hand2.toLowerCase().trim();
   // Write code here
+  if (hand1 === hand2) {
+    return 'It\'s a tie!';
 
+  } else if (hand1 === 'rock') {
+    if (hand2 === 'scissors') {
+      return "Hand one wins!";
+    } else {
+      return "Hand two wins!";
+    }
+
+  } else if (hand1 === 'paper') {
+    if (hand2 === 'rock') {
+      return "Hand one wins!";
+    } else {
+      return "Hand two wins!";
+    }
+
+  } else if (hand1 === 'scissors') {
+    if (hand2 === 'paper') {
+      return "Hand one wins!";
+    } else {
+      return "Hand two wins!";
+    }
+  }
 }
 
 function getPrompt() {
@@ -22,6 +46,13 @@ function getPrompt() {
     });
   });
 }
+
+
+
+
+
+
+
 
 // Tests
 
